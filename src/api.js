4376817ade85a35b4fca7d952e122e15c7122b8d
Alex Bujenita <dekc89@gmail.com>
2019-04-15 17:52:44 +0100

    select element

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 class ServerApi {
 
-  static Base_url = 'http://localhost:3000/users/';
+  static Base_url = 'http://localhost:3000/users';
 
   static getUserData(userId){
-      return this.get(this.Base_url + userId);
+      return this.get(this.Base_url + '/' + userId);
   }
 
   static get(url) {
     return fetch(url).then(resp => resp.json());
+  }
+
+  static getUsers() {
+    return fetch(this.Base_url).then(resp => resp.json())
   }
 
 
--- src/form.js
+++ src/form.js
@@ -1,11 +1,39 @@
 const ROOT = document.getElementById('root')
+let clicked = false
+
+
 
 const createForm = () => {
+
   const FORM_CONTAINER = document.createElement('div')
-  FORM_CONTAINER.className = 'form-container'
+  FORM_CONTAINER.className = 'hidden-form'
+  FORM_CONTAINER.id = 'form-container'
 
   const FORM = document.createElement('form')
   FORM.id = 'new-task-form'
+  FORM.addEventListener('submit', e => {
+    e.preventDefault()
+    const newTask = {
+      user_id: 1,
+      content: e.target.content.value,
+      end_date: e.target.date.value
+    }
+    console.log(newTask);
+
+    FORM.reset();
+    FORM_CONTAINER.className = 'hidden-form'
+    clicked = !clicked
+    
+  })
+
+  const TASK_CONTENT_LABEL = document.createElement('label')
+  TASK_CONTENT_LABEL.setAttribute('for', 'content')
+  TASK_CONTENT_LABEL.innerText = 'Please describe the task:'
+  const TASK_CONTENT = document.createElement('textarea')
+  TASK_CONTENT.rows = 5
+  TASK_CONTENT.minLength = 1
+  TASK_CONTENT.name = 'content'
+  TASK_CONTENT.placeholder = 'Type your task in here...'
 
   const TASK_DATE_LABEL = document.createElement('label')
   TASK_DATE_LABEL.setAttribute('for', 'End Date')
@@ -13,15 +41,43 @@ const createForm = () => {
 
   const TASK_DATE = document.createElement('input')
   TASK_DATE.type = 'date'
+  TASK_DATE.name = 'date'
+
+  const ASSIGN_USER = document.createElement('select')
+  ASSIGN_USER.id = 'user-selector'
+  ServerApi.getUsers().then(users => {
+    users.forEach(user => {
+      const OPTION = document.createElement('option')
+      OPTION.value = user.id
+      OPTION.innerText = user.name
+      ASSIGN_USER.appendChild(OPTION)
+    })
+  });
 
   const BUTTON = document.createElement('button')
   BUTTON.innerText = 'Create Task'
 
+  FORM.appendChild(TASK_CONTENT_LABEL)
+  FORM.appendChild(TASK_CONTENT)
   FORM.appendChild(TASK_DATE_LABEL)
   FORM.appendChild(TASK_DATE)
+  FORM.appendChild(ASSIGN_USER)
   FORM.appendChild(BUTTON)
 
   FORM_CONTAINER.appendChild(FORM)
 
   ROOT.appendChild(FORM_CONTAINER)
+}
+
+const showHideForm = () => {
+  const FORM = document.getElementById('form-container')
+
+  if (clicked) {
+    FORM.className = 'hidden-form'
+  } else {
+    FORM.className = 'show-form'
+  }
+
+  clicked = !clicked
+
 }
